371079dc6f10e866ce9e67bd56d6e8fd39aaeaaf
Vjacheslav <goplit2010.konovalov@yandex.ru>
2021-10-05 18:31:24 +0300

    delete magic and corect lint

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from page_loader.download import download, generate_name, KnownError, load_files
 import tempfile
 import pathlib
@@ -6,7 +7,7 @@ import pytest
 import os
 
 
-def test_downloader():
+def test_downloader() -> None:
     with tempfile.TemporaryDirectory() as tmp:   
         path_dir = pathlib.Path(tmp)
         path_page, path_to_folder = download('http://milk.com/' , path_dir)[2:]
@@ -24,18 +25,18 @@ def test_downloader():
     ('https://github.com/K0Hb/python-project-lvl3.css',
     'github-com-K0Hb-python-project-lvl3.css', 'file')
 ])
-def test_get_name(URL, get_name, status):
+def test_get_name(URL: str, get_name: str, status: str) -> None:
     assert generate_name(URL, status) == get_name
 
 
-def test_connection_failed():
+def test_connection_failed() -> None:
     with tempfile.TemporaryDirectory() as temp:
         with pytest.raises(KnownError) as e_info:
             download('http://httpbin.org/status/404', temp)
         assert 'Connection failed' in str(e_info.value)
 
 
-def test_load_files():
+def test_load_files() -> None:
     with tempfile.TemporaryDirectory() as temp:
         link_for_test = 'https://github.com/K0Hb/python-project-lvl3'
         path = os.path.join(temp, generate_name(link_for_test, 'file'))
@@ -43,25 +44,25 @@ def test_load_files():
         assert os.path.isfile(path)
 
 
-def test_error_no_schema():
+def test_error_no_schema() -> None:
     with pytest.raises(KnownError) as e_info:
         download('K0Hb.github.io/github.io/', '/fantom_path/')
     assert 'Wrong address!' in str(e_info.value)
 
 
-def test_error_invalid_schema():
+def test_error_invalid_schema() -> None:
     with pytest.raises(KnownError) as e_info:
         download('ht://K0Hb.github.io/github.io/', '/fantom_path/')
     assert 'Wrong address!' in str(e_info.value)
 
 
-def test_error_200():
+def test_error_200() -> None:
     with pytest.raises(KnownError) as e_info:
         download('http://httpbin.org/status/404', '/fantom_path/')
     assert 'Connection failed' in str(e_info.value)
 
 
-def test_error_umreal_folder():
+def test_error_umreal_folder() -> None:
     with pytest.raises(KnownError) as e_info:
         download('https://github.com/K0Hb/python-project-lvl3', 'unreal_path_to_file')
     assert 'Your folder is incorrect' in str(e_info.value)
