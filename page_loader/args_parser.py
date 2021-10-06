343e394ded841945472dd12bc7abf89c07138e73
Vjacheslav <goplit2010.konovalov@yandex.ru>
2021-10-06 22:53:52 +0300

    Edit structura parser

--- page_loader/args_parser.py
+++ page_loader/args_parser.py
@@ -10,11 +10,12 @@ DEBUG, INFO, WARNING, ERROR, CRITICAL = \
 def args_parser():
     parser = argparse.ArgumentParser(description='Loads the URL of the page\
          to a file at the specified PATH')
-    parser.add_argument('--output', default=os.getcwd())
+    parser.add_argument('-o', '--output', default=os.getcwd(), type=str,
+                        help='folder for saving link')
     parser.add_argument(
           '-l', '--level',
           type=str, default=INFO,
           choices=[DEBUG, INFO, WARNING, ERROR, CRITICAL],
           help='level of logging')
-    parser.add_argument('URL')
+    parser.add_argument('URL', type=str)
     return parser.parse_args()
