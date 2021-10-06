3673a0fd5042da6af21ca44f41ed54e85c270e3c
Vjacheslav <goplit2010.konovalov@yandex.ru>
2021-10-06 22:47:22 +0300

    change cli

--- page_loader/args_parser.py
+++ page_loader/args_parser.py
@@ -11,10 +11,10 @@ def args_parser():
     parser = argparse.ArgumentParser(description='Loads the URL of the page\
          to a file at the specified PATH')
     parser.add_argument('--output', default=os.getcwd())
-    parser.add_argument('URL')
     parser.add_argument(
           '-l', '--level',
           type=str, default=INFO,
           choices=[DEBUG, INFO, WARNING, ERROR, CRITICAL],
           help='level of logging')
+    parser.add_argument('URL')
     return parser.parse_args()
