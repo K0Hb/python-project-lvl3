3a0c1d01a5997fae08acb40e5be60596a830f84e
Vjacheslav <goplit2010.konovalov@yandex.ru>
2021-10-06 17:51:19 +0300

    edit return download

--- page_loader/__init__.py
+++ page_loader/__init__.py
@@ -23,4 +23,4 @@ def download(url: str, path: str) -> tuple:
 
     load_files(source_of_files)
 
-    return path_to_page, path_to_folder
+    return path_to_page
--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -13,9 +13,8 @@ def main() -> None:
     level_log = namespace.level
     modification_level(level_log)
     try:
-        file_path, path_to_folder = download(url, path)
+        file_path = download(url, path)
         print(f'Page saved in {file_path}')
-        print(f'Resources saved in {path_to_folder}')
     except KnownError:
         logging.error('Error')
         sys.exit(1)
